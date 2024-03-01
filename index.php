bdd9278ddc41058a91b555b0e07a63aae716624b
ItsTippy <beni1127lampu@gmail.com>
2024-03-01 09:11:01 +0700

    balik

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $run_sql = mysqli_query($is_connect,$query);
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Aplikasi Permudah Tinggalkan Kelaz</title>
+  <title>Aplikasi Permudah Tinggalkan Kelas</title>
   <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
   <link rel="stylesheet" href="../assets/css/styles.min.css" />
 </head>
